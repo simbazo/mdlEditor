1ea410e3d58a159ea861daaa72325e1ba79baf5b
simbazo <rory.leisegang@gmail.com>
2017-05-19 16:55:56 +0200

    uplosds Deleteds
    Changes

--- assets/treeview/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery.ajax({
-		url: "tree_data.php",
-		cache: false,
-		success: function(response){
-			$('#treeview').treeview({data: response});
-		}
-	});	
-});
