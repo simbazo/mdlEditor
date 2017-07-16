7bc68d0b646f0b3b741dae5b9a1bbf97041b0db5
Andrew Browne <mail.andrew.browne@gmail.com>
2017-07-17 00:25:53 +0200

    Made further changes to tusers
    Changes are:
    dob is now date field
    sex is now integer (one-to-one relationship with tsexes)
    email is unique and not nullable
    mobile is unique and not nullable
    secret_question changed to security_question
    secret_answer changed to security_answer
    added status_uuid (one-to-one relationship with tstatuses)

---
+++ core/database/migrations/2017_07_16_184345_changetable_structure_tusers.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangetableStructureTusers extends Migration
+{
+    /**
+     * Run the migration to update the tusers table.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('dob')->nullable(false)->change(); //CHANGED dob FROM STRING TO DATE
+            $table->integer('sex')->nullable(false)->change();//CHANGED sex FROM STRING TO INTEGER SO IT BECOMES A LOOKUP TO A tSex TABLE - DB NORMALISATION 
+            $table->string('email')->unique()->nullable(false)->change(); 
+            $table->string('mobile')->unique()->nullable(false)->change();
+            $table->renameColumn('secret_question', 'security_question');
+            $table->renameColumn('secret_answer', 'security_answer'); 
+            $table->integer('status_uuid')->nullable(false);//ADDED THIS AS A LOOKUP TO A tStaus TABLE
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
